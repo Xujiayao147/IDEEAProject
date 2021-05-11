42b41b41525e121a44ad122332f0d32c79845590
Xujiayao147 <caiandxu@163.com>
2021-05-11 22:08:54 +0800

    IDEEA项目不做了，这个仓库就先空着吧

--- README.md
+++ README.md
@@ -1,5 +1,3 @@
-# IDEEAProject
+# Untitled
 
-IDEEA动态网页项目 - Java
-
-非常感谢 [lenve/VBlog](https://github.com/lenve/VBlog) 的原始源代码，该项目也是根据 MIT 协议许可开源。
+Untitled - Java
--- vue/src/utils/utils.js
+++
@@ -1,9 +0,0 @@
-export const isNotNullORBlank = (...args) => {
-	for (let i = 0; i < args.length; i++) {
-		let argument = args[i];
-		if (argument == null || argument === '' || argument === undefined) {
-			return false;
-		}
-	}
-	return true;
-}
